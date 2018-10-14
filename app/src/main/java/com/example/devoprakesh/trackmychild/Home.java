9e46426df517d4b422b748c17c4ba9d2e9558fb3
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2018-10-14 10:07:13 +0530

    home to display

--- app/src/main/java/com/example/devoprakesh/trackmychild/Home.java
+++ app/src/main/java/com/example/devoprakesh/trackmychild/Home.java
@@ -1,5 +1,6 @@
 package com.example.devoprakesh.trackmychild;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent = new Intent(Home.this,Display.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/devoprakesh/trackmychild/SplashScreen.java
+++ app/src/main/java/com/example/devoprakesh/trackmychild/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.devoprakesh.trackmychild;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Handler;
@@ -15,10 +16,14 @@ import android.widget.Toast;
 public class SplashScreen extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST=1;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        sharedPreferences = getSharedPreferences("UserLoginDetails",MODE_PRIVATE);
 
         LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
         if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
@@ -37,6 +42,12 @@ public class SplashScreen extends AppCompatActivity {
                 @Override
                 public void run() {
 
+                    if(sharedPreferences.getString("UID",null)!= null){
+
+                        Intent intent = new Intent(SplashScreen.this,Register.class);
+                        startActivity(intent);
+                        finish();
+                    }
                     Intent intent = new Intent(SplashScreen.this,Register.class);
                     startActivity(intent);
                     finish();
