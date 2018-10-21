3d6bb88fb495c641dddf84d5ca4a9dc2fdb5daad
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2018-10-21 20:08:23 +0530

    new addon 0990 Custom Adaptor

--- app/src/main/java/com/example/devoprakesh/trackmychild/TrackListAdaptor.java
+++ app/src/main/java/com/example/devoprakesh/trackmychild/TrackListAdaptor.java
@@ -1,6 +1,35 @@
 package com.example.devoprakesh.trackmychild;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class TrackListAdaptor  {
+public class TrackListAdaptor extends RecyclerView.Adapter<TrackListAdaptor.ViewHolder> {
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
